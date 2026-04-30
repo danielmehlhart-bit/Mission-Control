38305b350715ba9183c90d650a6b7c6f09dfbd64
Hatti <hatti@hartner.dev>
2026-04-30 12:28:47 +0200

    auto-sync 2026-04-30 12:28

--- components/voice/voice-console.tsx
+++ components/voice/voice-console.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 type VoiceProfileSummary = {
   id: string;
